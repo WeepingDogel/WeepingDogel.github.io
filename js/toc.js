a8312e1f7f67004da58d30f38222b82757f80b19
WeepingDogel <weepingdogel@gmail.com>
2021-07-08 13:53:44 +0800

    修正

--- js/toc.js
+++ js/toc.js
@@ -1,55 +1,74 @@
 var spy = function () {
-    var elems = $(":header");
-    if (elems.length == 0) {
-        return;
-    }
-    var currentTop = $(window).scrollTop();
-    var currentBottom = $(window).scrollTop() + $(window).height();
-    var pageBottom = $('#EOF').offset().top;
+  var elems = document.querySelectorAll("h1, h2, h3, h4, h5, h6");
+  if (elems.length == 0) {
+    return;
+  }
+  var supportPageOffset = window.pageXOffset !== undefined;
+  var isCSS1Compat = ((document.compatMode || "") === "CSS1Compat");
 
-    var meetUnread = false
-    var currentIndex = -1
-    elems.each(function (idx) {
-        var elemTop = $(this).offset().top;
-        var id = $(this).attr('id');
-        var navElem = $('#' + id + '-nav');
-        if (currentTop + $(this).height() >= elemTop || currentBottom >= pageBottom) {
-            navElem.addClass('toc-active');
-        } else {
-            if (meetUnread == false) {
-                meetUnread = true
-                currentIndex = idx - 1
-            }
-            navElem.removeClass('toc-active');
+  var currentTop = supportPageOffset ? window.pageYOffset : isCSS1Compat ? document.documentElement.scrollTop : document.body.scrollTop;
+  var currentBottom =  currentTop + window.height;
+  var pageBottom = window.pageBottom;
+
+  var meetUnread = false
+  var lastId = ""
+  elems.forEach(function (elem, idx) {
+    var elemTop = elem.offsetTop;
+    var id = elem.getAttribute('id');
+    //console.log(id,elem.offsetTop)
+    var navElem = document.getElementById(id + '-nav');
+    if (navElem==null) {
+      return
+    }
+    if (currentTop >= elemTop || currentBottom >= pageBottom) {
+      navElem.classList.add('toc-active');
+    } else {
+      if (meetUnread == false) {
+        meetUnread = true;
+        try{
+          document.getElementById(elems[idx-1].id+"-nav").scrollIntoView({ block:"center", behavior: 'smooth' });
+        } catch {
+
         }
-    })
+        
+      }
+      navElem.classList.remove('toc-active');
+    }
+  })
+
 }
-$().ready(function () {
-    $(".collapse").each(function (idx) {
-        $(this).collapse("show");
-    });
-    spy();
-    $(window).bind('scroll', throttle(spy));
-});
+
+var onNavClick = function () {
+  
+}
+
+
+/*$().ready(function () {
+  $(".collapse").each(function (idx) {
+    $(this).collapse("show");
+  });
+  //spy();
+  //$(window).bind('scroll', throttle(spy));
+});*/
 
 function throttle(func, timeout = 250) {
-    let last;
-    let timer;
-   
-    return function () {
-      const context = this;
-      const args = arguments;
-      const now = +new Date();
-   
-      if (last && now < last + timeout) {
-        clearTimeout(timer)
-        timer = setTimeout(function () {
-          last = now
-          func.apply(context, args)
-        }, timeout)
-      } else {
+  let last;
+  let timer;
+
+  return function () {
+    const context = this;
+    const args = arguments;
+    const now = +new Date();
+
+    if (last && now < last + timeout) {
+      clearTimeout(timer)
+      timer = setTimeout(function () {
         last = now
         func.apply(context, args)
-      }
+      }, timeout)
+    } else {
+      last = now
+      func.apply(context, args)
     }
   }
+}
